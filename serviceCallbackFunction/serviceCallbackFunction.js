8dc54c12e4a4768821e01f40956681de5590fceb
Anil Dhawale <anil.dhawale@hmcts.net>
2022-03-01 09:58:50 +0000

    bump up property volume version to 0.0.13

--- serviceCallbackFunction/serviceCallbackFunction.js
+++ serviceCallbackFunction/serviceCallbackFunction.js
@@ -12,6 +12,7 @@ const delayTime = config.get('delayMessageMinutes');
 
 const s2sUrl = config.get('s2sUrl');
 const s2sSecret = config.get('secrets.ccpay.payment-s2s-secret');
+const testObj = config.get('secrets.ccpay.payment-s2s-secret');
 const microService = config.get('microservicePaymentApp');
 
 
@@ -20,6 +21,7 @@ const MAX_RETRIES = 3;
 
 module.exports = async function serviceCallbackFunction() {
     console.log('microService for testing :' + microService);
+    console.log('testObj :' + testObj);
     const sbClient = ServiceBusClient.createFromConnectionString(connectionString);
     const subscriptionClient = sbClient.createSubscriptionClient(topicName, subscriptionName);
     const receiver = subscriptionClient.createReceiver(ReceiveMode.peekLock);
