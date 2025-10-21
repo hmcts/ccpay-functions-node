9c4980891eca0cad7394a6ea06cc952a48a6e294
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2025-10-21 13:28:04 +0100

    PAY-7153 await async operations to prevent message double-settlement

--- serviceCallbackFunction/serviceCallbackFunction.js
+++ serviceCallbackFunction/serviceCallbackFunction.js
@@ -30,6 +30,7 @@ module.exports = async function serviceCallbackFunction() {
         let serviceName;
         let correlationId = msg.correlationId === undefined ? randomInt(100000,999999) : msg.correlationId;
         msg.correlationId = correlationId;
+        let messageSettled = false;
         try {
             if (this.validateMessage(msg)) {
                 serviceCallbackUrl = msg.userProperties.serviceCallbackUrl;
@@ -41,10 +42,8 @@ module.exports = async function serviceCallbackFunction() {
                     oneTimePassword: otpPassword
                 };
 
-                axiosRequest.post(
-                    s2sUrl + '/lease',
-                    serviceAuthRequest
-                ).then(token => {
+                try {
+                    const token = await axiosRequest.post(s2sUrl + '/lease', serviceAuthRequest);
                     console.log(correlationId + ': S2S Token Retrieved.......');
                     const options = {
                         headers: {
@@ -56,35 +55,28 @@ module.exports = async function serviceCallbackFunction() {
                         console.log(correlationId + ': Headers: ', Buffer.from(JSON.stringify(options)).toString("base64"));
                     }
                     console.log(correlationId + ': About to post callback URL ', serviceCallbackUrl);
-                    axiosRequest.put(
-                        serviceCallbackUrl,
-                        msg.body,
-                        options
-                    ).then(response => {
-                        console.log(correlationId + ': Response: ' + JSON.stringify(response.data));
-                        if(response && response.status >= 200 && response.status < 300) {
-                            console.log(correlationId + ': Message Sent Successfully to ' + serviceCallbackUrl);
-                        } else {
-                            console.log(correlationId + ': Error in Calling Service ' + JSON.stringify(response));
-                            retryOrDeadLetter(msg);
-                        }
-                    }).catch((callbackError) => {
-                        console.log(correlationId + ': Error in fetching callback request ' + callbackError);
-                        retryOrDeadLetter(msg);
-                    });
-                }).catch((s2sError) => {
-                    console.log(correlationId + ': Error in fetching S2S token message ' + s2sError);
-                    retryOrDeadLetter(msg);
-                });
+                    const response = await axiosRequest.put(serviceCallbackUrl, msg.body, options);
+                    console.log(correlationId + ': Response: ' + JSON.stringify(response.data));
+                    if(response && response.status >= 200 && response.status < 300) {
+                        console.log(correlationId + ': Message Sent Successfully to ' + serviceCallbackUrl);
+                    } else {
+                        console.log(correlationId + ': Error in Calling Service ' + JSON.stringify(response));
+                        messageSettled = await retryOrDeadLetter(msg);
+                    }
+                } catch (requestError) {
+                    console.log(correlationId + ': Error in request: ' + requestError);
+                    messageSettled = await retryOrDeadLetter(msg);
+                }
             } else {
                 console.log(correlationId + ': Skipping processing invalid message and sending to dead letter' + JSON.stringify(msg.body));
                 await msg.deadLetter();
+                messageSettled = true;
             }
         } catch (err) {
             console.log(correlationId + ': Error response received from ', serviceCallbackUrl, err);
-          retryOrDeadLetter(msg);
+            messageSettled = await retryOrDeadLetter(msg);
         } finally {
-            if (!msg.isSettled) {
+            if (!messageSettled && !msg.isSettled) {
                 await msg.complete();
             }
         }
@@ -94,24 +86,26 @@ module.exports = async function serviceCallbackFunction() {
     await sbClient.close();
 }
 
-retryOrDeadLetter = msg => {
+retryOrDeadLetter = async (msg) => {
     let correlationId = msg.correlationId;
     if (!msg.userProperties.retries) {
         msg.userProperties.retries = 0;
     }
     if (msg.userProperties.retries === MAX_RETRIES) {
         console.log(correlationId + ": Max number of retries reached for ", JSON.stringify(msg.body));
-        msg.deadLetter()
-            .then(() => {
-                console.log(correlationId + ": Dead lettered a message ", JSON.stringify(msg.body));
-            })
-            .catch(err => {
-                console.log(correlationId + ": Error while dead letter messages ", err)
-            });
+        try {
+            await msg.deadLetter();
+            console.log(correlationId + ": Dead lettered a message ", JSON.stringify(msg.body));
+            return true;
+        } catch (err) {
+            console.log(correlationId + ": Error while dead letter messages ", err);
+            return false;
+        }
     } else {
         console.log(correlationId + ": Will retry message at a later time ", JSON.stringify(msg.body));
         msg.userProperties.retries++;
-        sendMessage(msg.clone(), correlationId);
+        await sendMessage(msg.clone(), correlationId);
+        return false;
     }
 }
 
@@ -146,16 +140,13 @@ async function sendMessage(msg, correlationId) {
     const topicSender = topicClient.createSender();
     const msgFailedTime = new Date();
     const retryLaterTime = new Date(msgFailedTime.setMinutes(msgFailedTime.getMinutes() + parseInt(delayTime)));
-    topicSender.scheduleMessage(retryLaterTime, msg)
-        .then(() => {
-            console.log(correlationId + ": Message is scheduled to retry at UTC: ", retryLaterTime);
-        })
-        .catch(err => {
-            console.log(correlationId + ": Error while scheduling message ", err)
-        }).finally(() => {
-            (async () => {
-                await topicClient.close();
-                await sBusClient.close();
-            })();
-        })
+    try {
+        await topicSender.scheduleMessage(retryLaterTime, msg);
+        console.log(correlationId + ": Message is scheduled to retry at UTC: ", retryLaterTime);
+    } catch (err) {
+        console.log(correlationId + ": Error while scheduling message ", err);
+    } finally {
+        await topicClient.close();
+        await sBusClient.close();
+    }
 }
