79f8d38e1ef98271b1605dcf500b6ef55cdaeec8
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2025-12-23 11:21:36 +0000

    CME-851 sampling at 1%

--- serviceCallbackFunction/index.js
+++ serviceCallbackFunction/index.js
@@ -3,12 +3,14 @@ const appInsights = require("applicationinsights");
 const config = require('config');
 
 appInsights.setup(config.get('appInsightsInstumentationKey'))
+    .setAutoDependencyCorrelation(true)
     .setAutoCollectConsole(true, true)
     .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-    .setSendLiveMetrics(true)
-    .start();
+    .setSendLiveMetrics(true);
 appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'ccpay-callback-function';
 appInsights.defaultClient.config.maxBatchSize = 0;
+appInsights.defaultClient.config.samplingPercentage = 1;
+appInsights.start();
 serviceCallbackFunction().catch((err) => {
   console.log("Error occurred: ", err);
 });
